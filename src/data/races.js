21d5f18f6c098e44e840799d1edb6dee96a40d93
Hamza Naceur <hamzanaceur@MacBook-Air-de-Hamza.local>
2023-01-05 17:41:04 +0100

    fix: bug races array

--- src/data/races.js
+++ src/data/races.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-anonymous-default-export */
-git export default [
-    { value: 'human', label: 'Humain' },
-    { value: 'elf', label: 'Elfe' },
-    { value: 'dwarf', label: 'Nain' },
+export default [
+    { value: 'human', label: 'Human' },
+    { value: 'elf', label: 'Elf' },
+    { value: 'dwarf', label: 'Dwarf' },
     { value: 'orc', label: 'Orc' },
     { value: 'goblin', label: 'Gobelin' },
     { value: 'tauren', label: 'Tauren' },
     { value: 'gnome', label: 'Gnome' },
-    { value: 'harpy', label: 'Harpie' },
-    { value: 'centaur', label: 'Centaure' },
+    { value: 'harpy', label: 'Harpy' },
+    { value: 'centaur', label: 'Centaur' },
   ];
